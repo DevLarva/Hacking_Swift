988bc1f66b6f61a519d1335ef9ca107475805feb
bdh3620 <bdh3620@naver.com>
2022-09-07 15:19:57 +0900

    Section 수정, 카페인 수량 수정

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,79 +10,85 @@ import SwiftUI
 struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    @State private var coffeeAmount = 0
     
     @State private var aleatTitle = ""
     @State private var aleatMessage = ""
     @State private var showingAlert = false
     
+    var correctedCoffeeAmount: Int {
+        coffeeAmount + 1
+    }
+    
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
         components.minute = 0
+        
         return Calendar.current.date(from: components) ?? Date.now
     }
     
     var body: some View {
-        NavigationView {
-            Form {
-                VStack(alignment: .leading, spacing: 0) {
-                Text("몇시에 기상하고 싶으신가요?")
-                    .font(.headline)
+            NavigationView {
+                Form {
+                    Section {
+                    Text("몇시에 기상하고 싶으신가요?")
+                        .font(.headline)
 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                }
-                
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text("원하는 수면시간을 선택해주세요.")
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                    }
+                    
+                    Section {
+                        Text("원하시는 수면시간을 선택해주세요.")
+                                .font(.headline)
+                            
+                            Stepper("\(sleepAmount.formatted())시간", value: $sleepAmount, in: 4...12, step: 0.25)
+                        }
+                    
+                    Section {
+                        Text("하루 동안 몇잔의 커피를 드시나요?")
                             .font(.headline)
                         
-                        Stepper("\(sleepAmount.formatted())시간", value: $sleepAmount, in: 4...12, step: 0.25)
+                        Stepper(coffeeAmount == 1 ? "1 컵" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
                     }
-                
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("카페인 섭취량")
-                        .font(.headline)
-                    
-                    Stepper(coffeeAmount == 1 ? "1 컵" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
+                .navigationTitle("BetterRest")
+                .toolbar {
+                    Button("계산하기",action: caculateBedtime)
+                }
+                .alert(aleatTitle,isPresented: $showingAlert) {
+                    Button("확인") { }
+                } message: {
+                    Text(aleatMessage)
                 }
             }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Caculate",action: caculateBedtime)
-            }
-            .alert(aleatTitle,isPresented: $showingAlert) {
-                Button("확인") { }
-            } message: {
-                Text(aleatMessage)
+        }
+            func caculateBedtime() {
+                do {
+                    let config = MLModelConfiguration()
+                    let model = try SleepCalculator(configuration: config)
+                    
+                    let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+                    let hour = (components.hour ?? 0) * 60 * 60
+                    let minute = (components.minute ?? 0) * 60
+                    
+                    let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double((coffeeAmount)))
+                    
+                    let sleepTime = wakeUp - prediction.actualSleep
+                    aleatTitle = "당신의 가장 이상적인 취침 시간은"
+                    aleatMessage = sleepTime.formatted(date:.omitted,time: .shortened)
+                } catch {
+                    aleatTitle = "오류"
+                    aleatMessage = "죄송합니다.문제가 발생 하였습니다."
+                }
+                showingAlert = true
             }
         }
-    }
-    func caculateBedtime() {
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double((coffeeAmount)))
-            
-            let sleepTime = wakeUp - prediction.actualSleep
-            aleatTitle = "당신의 가장 이상적인 취침 시간은"
-            aleatMessage = sleepTime.formatted(date:.omitted,time: .shortened)
-        } catch {
-            aleatTitle = "오류"
-            aleatMessage = "죄송합니다.문제가 발생 하였습니다."
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
         }
-        showingAlert = true
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
