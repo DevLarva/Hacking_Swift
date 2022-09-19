ed152ff6da24780993e44eda0f7c49257d7a109f
백대홍 <bdh3620@naver.com>
2022-09-19 21:02:35 +0900

    git Test

---
+++ xcode/Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by 백대홍 on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
